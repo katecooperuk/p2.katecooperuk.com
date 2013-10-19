6c3f5379a849eb4435c4a3748e20ce67b46a6643
Kate Cooper <kate.cooper@mac.com>
2013-10-19 21:08:00 +0100

    Class 6: view instance with $user_name

--- controllers/c_users.php
+++ controllers/c_users.php
@@ -25,13 +25,14 @@ class users_controller extends base_controller {
     }
 
     public function profile($user_name = NULL) {
-
-        if($user_name == NULL) {
-            echo "No user specified";
-        }
-        else {
-            echo "This is the profile for ".$user_name;
-        }
+    
+    	if($user_name == NULL) {
+	    	echo "No user specified";
+    	}
+    	else {
+	    	echo "This is the profile for ".$user_name;
+    	}
     }
-
+    
+    
 } # end of the class
