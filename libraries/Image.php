c1195c2147726749468b32b339f14e9fb19579ce
Kate Cooper <kate.cooper@mac.com>
2013-10-19 20:20:36 +0100

    Class 6: Adding notes regarding Class properties

---
+++ libraries/Image.php
@@ -0,0 +1,23 @@
+<?php
+
+class Image {
+
+	/*
+    Class properties
+    Encapsulation:
+    Access modifiers indicate what access levels other classes can have to these properties
+    1) Public    - Any other class can access the property
+    2) Private   - Only this class can access the property
+    3) Protected - This class and any class that inherits it can access this property
+    */
+
+	protected $image;
+	protected $width;
+	protected $height;
+	protected $mimetype;
+	
+	
+	
+	
+	
+}	# eoc
