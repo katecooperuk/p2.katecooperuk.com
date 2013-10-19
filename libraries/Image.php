9c12a9602ea3388dff1d890c14264ce78b16958b
Kate Cooper <kate.cooper@mac.com>
2013-10-19 20:23:57 +0100

    Class 6: Adding function - display, resize and construct

--- libraries/Image.php
+++ libraries/Image.php
@@ -16,8 +16,19 @@ class Image {
 	protected $height;
 	protected $mimetype;
 	
+	public function display() {
+		header("Content-type: {$this->mimetype}");
+        switch($this->mimetype) {
+            case 'image/jpeg': imagejpeg($this->image); break;
+            case 'image/png': imagepng($this->image); break;
+            case 'image/gif': imagegif($this->image); break;
+        }
+	}
 	
-	
-	
+	public function resize($width, $height) {
+        $thumb = imagecreatetruecolor($width, $height);
+        imagecopyresampled($thumb, $this->image, 0, 0, 0, 0, $width, $height, $this->width, $this->height);
+        $this->image = $thumb;
+    }	
 	
 }	# eoc
