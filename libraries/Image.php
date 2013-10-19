e68d620a63cb2564b2afb0c8212fe42beccbdd86
Kate Cooper <kate.cooper@mac.com>
2013-10-19 20:25:49 +0100

    Class 6: Adding function - construct + notes

--- libraries/Image.php
+++ libraries/Image.php
@@ -16,6 +16,33 @@ class Image {
 	protected $height;
 	protected $mimetype;
 	
+	 /* 
+    Constructor
+    __construct is a Magic method (optional), called whenever an object is instantiated
+    Magic methods are reserved methods built into PHP, and prefixed with two underscores
+    */
+    public function __construct($filename) {
+
+        # Read the image file to a binary buffer
+        $fp  = fopen($filename, 'rb') or die("Image '$filename' not found!");
+        $buf = '';
+        while(!feof($fp))
+            $buf .= fgets($fp, 4096);
+
+        /*
+        Create image and asign it to the image property
+        $this is a built in variable that points to the current object. 
+        It's used to access properties and methods of the current class.
+        */
+        $this->image = imagecreatefromstring($buf);
+
+        # Extract image information storing in the class attributes
+        $info           = getimagesize($filename);
+        $this->width    = $info[0];
+        $this->height   = $info[1];
+        $this->mimetype = $info['mime'];
+    }
+	
 	public function display() {
 		header("Content-type: {$this->mimetype}");
         switch($this->mimetype) {
