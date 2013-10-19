b510437400cbba748c6554eaee06fa38d414aa07
Kate Cooper <kate.cooper@mac.com>
2013-10-19 20:30:11 +0100

    Class 6: Practice test1 - notes for testing on localhost

---
+++ controllers/c_practice.php
@@ -0,0 +1,11 @@
+<?php
+class practice_controller extends base_controller {
+	
+		public function test1() {
+		
+			# Test: localhost/practice/test1
+			echo "You are looking at test1";
+		}
+	
+	
+}
