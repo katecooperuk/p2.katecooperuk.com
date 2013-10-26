091ef1677c72fbfffa62a768e3fddddcb99c4353
Kate Cooper <kate.cooper@mac.com>
2013-10-26 09:43:07 +0100

    Class 7: Database insert test

--- controllers/c_practice.php
+++ controllers/c_practice.php
@@ -1,6 +1,21 @@
 <?php
 class practice_controller extends base_controller {
 	
+		/* Database Insert Test - N.B. Use double quotes "" */
+		public function test_db() {
+			
+			$q = 'INSERT INTO users
+				SET first_name = "Albert",
+				last_name = "Einstein"';
+				
+			echo $q;
+			
+			# Runs through DB.php in core - puts Albert Einstein into users
+			DB::instance(DB_NAME)->query($q);
+			
+		}
+		
+		/*  Demonstrating Classes/Objects */
 		public function test1() {
 		
 			# access to class 
