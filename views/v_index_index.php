a9a0ec9fdf1476bf375670eb05a034fe0e20f365
Kate Cooper <kate.cooper@mac.com>
2013-10-26 18:26:04 +0100

    Class 8 - removed test from template

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -1,12 +1,9 @@
 <?php if($user): ?>
-
-	<pre>
-	<?php
-	print_r($user);
-	?>
-	</pre>
 	
 	Hello <?=$user->first_name;?>
+	
 <?php else: ?>
+
 	Welcome to my app.  Please sign up or log in
+	
 <?php endif; ?>
