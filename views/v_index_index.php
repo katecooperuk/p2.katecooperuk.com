52b2b63d777b15f6b47682b3c2f0a4f26f13da9c
Kate Cooper <kate.cooper@mac.com>
2013-10-26 14:36:05 +0100

    Class 8 - Updating index page - logged in or out

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -1,1 +1,5 @@
-<h1>Welcome to <?=APP_NAME?><?php if($user) echo ', '.$user->first_name; ?></h1>
+<?php if($user): ?>
+	Hello <?=$user->first_name;?>
+<?php else: ?>
+	Welcome to my app.  Please sign up or log in
+<?php endif; ?>
