092fba7a5131ff4251fd091fcdab3363ea6e8829
Kate Cooper <kate.cooper@mac.com>
2013-10-26 12:13:04 +0100

    Class 7: user signup form

--- views/v_users_signup.php
+++ views/v_users_signup.php
@@ -2,10 +2,21 @@
 
 <form method='POST' action='/users/p_signup'>
 
-	First Name <input type='text' name='first_name'><br>
-	Last Name <input type='text' name='last_name'><br>
-	Email <input type='text' name='email'><br>
-	Password <input type='password' name='password'><br>
+	First Name<br>
+	<input type='text' name='first_name'>
+	<br><br>
+	
+	Last Name<br>
+	<input type='text' name='last_name'>
+	<br><br>
+	
+	Email<br>
+	<input type='text' name='email'>
+	<br><br>
+	
+	Password<br>
+	<input type='password' name='password'>
+	<br><br>
 	
 	<input type='submit' value='Sign Up'>
 
