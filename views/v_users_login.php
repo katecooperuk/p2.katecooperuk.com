b33dcc662c5f03e3ead4a07f85bf40f6487fb365
Kate Cooper <kate.cooper@mac.com>
2013-11-02 18:36:49 +0000

    cannot get 'User already exists' announcement to appear on the redirect login page

--- views/v_users_login.php
+++ views/v_users_login.php
@@ -1,7 +1,7 @@
-
-
 <div class="formcontent">
-<h2><?php if(isset($user_exists)) echo 'This user already exists, please log in.'; ?><h2>
+
+	<h2><?php if(isset($user_exists)) echo 'This user already exists, please log in.'; ?><h2>
+	
 	<h2>LOG IN</h2>
 
 		<form method='POST' action='/users/p_login'>
