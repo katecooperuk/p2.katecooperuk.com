433b57a8a6a88af179dc5dff1eb56d2f6e66af28
Kate Cooper <kate.cooper@mac.com>
2013-11-02 14:04:30 +0000

    updating CSS code

--- views/v_posts_index.php
+++ views/v_posts_index.php
@@ -1,7 +1,11 @@
-<?php foreach($posts as $post): ?>
-
-	<strong><?=$post['first_name']?> posted:</strong><br>
-	<?=$post['content']?><br>
-	<i><?=Time::display($post['created'])?></i><br><br>
+<div class="content">	
 	
-<?php endforeach; ?>
+	<?php foreach($posts as $post): ?>
+
+		<strong><?=$post['first_name']?> posted:</strong><br>
+		<?=$post['content']?><br>
+		<i><?=Time::display($post['created'])?></i><br><br>
+	
+	<?php endforeach; ?>
+		
+</div>
