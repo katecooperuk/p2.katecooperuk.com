7d4cbee6167eca0bd07daae96568ebf7490a2565
Kate Cooper <kate.cooper@mac.com>
2013-11-02 12:44:49 +0000

    changed welcome message

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -1,9 +1,12 @@
 <?php if($user): ?>
 	
-	Hello <?=$user->first_name;?>
+	Hello <?=$user->first_name;?>, welcome back to ChatterBox!
 	
 <?php else: ?>
 
-	Welcome to ChatterBox.  Please sign up or log in
+	Welcome to ChatterBox.<br>
+	Login if you already have an account<br>
+	or sign up to create an account and join the chatter!
+	
 	
 <?php endif; ?>
