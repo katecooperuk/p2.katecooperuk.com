0f9c05a3f58129766ee09b5bc3fe4ce882254c67
Kate Cooper <kate.cooper@mac.com>
2013-11-02 13:56:53 +0000

    updated div tags and added rows to text area

--- views/v_posts_add.php
+++ views/v_posts_add.php
@@ -1,11 +1,13 @@
 <div class="formbox">
 
-	<form method='post' action='/posts/p_add'>
+	<h2>ChatterBox Post</h2>
 
-		<textarea name='content'></textarea>
-	
+	<form method='POST' action='/posts/p_add'>
+
+		<textarea name='content' rows="5"></textarea>
+		<br><br>
+		
 		<input type='Submit' value='Add new post'>
-
+		
 	</form>
 	
-</div>
