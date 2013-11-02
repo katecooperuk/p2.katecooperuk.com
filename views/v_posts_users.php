ca6c68c2c98ff05622cea7ab7bb38d14873c08b5
Kate Cooper <kate.cooper@mac.com>
2013-11-02 14:48:48 +0000

    updated CSS - changed class name

--- views/v_posts_users.php
+++ views/v_posts_users.php
@@ -1,14 +1,14 @@
-<?php foreach($users as $user): ?>
+<div class="content">
+	<?php foreach($users as $user): ?>
 
-	<?=$user['first_name']?> <?=$user['last_name']?><br>
+		<div id="name"><?=$user['first_name']?> <?=$user['last_name']?></div>
 	
-	<?php if(isset($connections[$user['user_id']])): ?>
-		<a href='/posts/unfollow/<?=$user['user_id']?>'>Unfollow</a>
-	<?php else: ?>
-		<a href='/posts/follow/<?=$user['user_id']?>'>Follow</a>
-	<?php endif; ?>
-	
-	<br>
-	<br>
+		<?php if(isset($connections[$user['user_id']])): ?>
+			<a href='/posts/unfollow/<?=$user['user_id']?>'>Unfollow</a>
+		<?php else: ?>
+			<a href='/posts/follow/<?=$user['user_id']?>'>Follow</a>
+		<?php endif; ?>
+		<br><br>
 
-<?php endforeach ?>
+	<?php endforeach ?>
+</div>
