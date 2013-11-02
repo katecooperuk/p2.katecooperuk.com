0a216b211a17d4c289809c81f037a75b29099a03
Kate Cooper <kate.cooper@mac.com>
2013-11-02 12:46:08 +0000

    added formbox div

--- views/v_posts_add.php
+++ views/v_posts_add.php
@@ -1,7 +1,11 @@
-<form method='post' action='/posts/p_add'>
+<div class="formbox">
 
-	<textarea name='content'></textarea>
+	<form method='post' action='/posts/p_add'>
+
+		<textarea name='content'></textarea>
 	
-	<input type='Submit' value='Add new post'>
+		<input type='Submit' value='Add new post'>
 
-</form>
+	</form>
+	
+</div>
