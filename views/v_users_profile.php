93db8cea4341e287c548bbbf42d32683ff1f7417
Kate Cooper <kate.cooper@mac.com>
2013-11-03 13:54:22 +0000

    +1 Feature - changed PHP + CSS for profile image and showing user's own posts

--- views/v_users_profile.php
+++ views/v_users_profile.php
@@ -1,5 +1,20 @@
 <div class="content">
 	
-	<h2>This is <?=$user->first_name?>'s Profile</h2>
+	<h2><?=$user->first_name?>'s Profile</h2>
+	<img src="<?=$user->avatar; ?>">
+	
+	<form method='POST' action="/users/picture/" enctype="multipart/form-data" >
+    	<input type='file' accept='image' name='avatar'><br>
+		<input type='submit' name='submit' value='Upload Avatar'>
+	</form>
+	
+	<h2>My Posts</h2>
+	<?php foreach($posts as $post): ?>
+	
+		<div id="post"><?=$post['content']?></div>
+		<div id="time"><?=Time::display($post['created'])?></div>
+		<br>
+	
+	<?php endforeach;?>  
 
 </div>
