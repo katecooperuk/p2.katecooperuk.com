50bf340fcc8ee57ec19709c52e60dd4c65b742cb
Kate Cooper <kate.cooper@mac.com>
2013-11-03 15:55:35 +0000

    added a link to Users if no followers

--- views/v_posts_index.php
+++ views/v_posts_index.php
@@ -1,5 +1,11 @@
 <div class="content">	
 	
+	<?php if (count($posts) == 0) :?>
+		<h4>You are not following anyone yet,<br>
+			choose some <a href="/posts/users/">ChatterBoxes</a>to follow
+		</h4>
+	<?php endif; ?>
+	
 	<?php foreach($posts as $post): ?>
 		<img class="posts" src="/uploads/avatars/<?=$post['avatar']?>" >
 		<div id="name"><?=$post['first_name']?></div>
