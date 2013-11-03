2346bcc09db1ade5220f2497dced97dd60d204f8
Kate Cooper <kate.cooper@mac.com>
2013-11-03 15:56:20 +0000

    added $client_files_head code which seems to have fixed a whole heap of problems

--- views/_v_template.php
+++ views/_v_template.php
@@ -57,6 +57,8 @@
 			<?php if(isset($content)) echo $content; ?>
 
 			<?php if(isset($client_files_body)) echo $client_files_body; ?>
+			
+			<?php if(isset($client_files_head)) echo $client_files_head; ?>
 	
 	</div>
 	
