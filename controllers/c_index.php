85693d121597293982e9f0b0ff66e2a9f53eaf9d
Kate Cooper <kate.cooper@mac.com>
2013-11-03 08:18:00 +0000

    cleaning up code

--- controllers/c_index.php
+++ controllers/c_index.php
@@ -20,15 +20,6 @@ class index_controller extends base_controller {
 			
 		# Now set the <title> tag
 			$this->template->title = "Welcome to ChatterBox";
-	
-		# CSS/JS includes
-			/*
-			$client_files_head = Array("");
-	    	$this->template->client_files_head = Utils::load_client_files($client_files);
-	    	
-	    	$client_files_body = Array("");
-	    	$this->template->client_files_body = Utils::load_client_files($client_files_body);   
-	    	*/
 	      					     		
 		# Render the view
 			echo $this->template;
