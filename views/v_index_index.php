8aec2a106fe210a92a4d2e72d0e45f087b30b08e
Kate Cooper <kate.cooper@mac.com>
2013-11-03 14:24:54 +0000

    added CSS div to text

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -1,19 +1,19 @@
-<div class="content">
+<div class="content-home">
 
 	<?php if($user): 
 		
 		# Send to Profile Page
 		Router::redirect('/users/profile');
 	?>
-	
-		
-	
+
 	<?php else: ?>
 
-		Welcome to ChatterBox.<br>
-		Login if you already have an account<br>
-		or sign up to create an account and join the chatter!
-	
+		<div id="welcome">
+			Welcome to ChatterBox.<br>
+			Login if you already have an account<br>
+			or sign up to create an account and<br>
+			join in the chatter!
+		</div>
 	
 	<?php endif; ?>
 
